b8e41a9540347de543bd1a923ac5588f35b7d920
jwinsl01 <jonathan.winsley@fiscal.treasury.gov>
2019-11-07 09:21:40 -0500

    Updates

---
+++ precache-manifest.a715435ada6ee25f9460570a8021bd62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c33618af03c84f022508f02c04d24ea",
+    "url": "/desk-clock/index.html"
+  },
+  {
+    "revision": "b02ee38be6354718b0f2",
+    "url": "/desk-clock/static/css/2.37afa4cc.chunk.css"
+  },
+  {
+    "revision": "a61a4c61bd7e7eceaa7a",
+    "url": "/desk-clock/static/css/main.98bbfc84.chunk.css"
+  },
+  {
+    "revision": "b02ee38be6354718b0f2",
+    "url": "/desk-clock/static/js/2.639d6350.chunk.js"
+  },
+  {
+    "revision": "a61a4c61bd7e7eceaa7a",
+    "url": "/desk-clock/static/js/main.774650d7.chunk.js"
+  },
+  {
+    "revision": "5c969b6568a23d09bbb0",
+    "url": "/desk-clock/static/js/runtime-main.898effa2.js"
+  }
+]);
