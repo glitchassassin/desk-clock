bcf04b7a8c5dcae93678da512de6e2f560c7b693
jwinsl01 <jonathan.winsley@fiscal.treasury.gov>
2019-11-07 17:54:33 -0500

    Removed console.log

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ class App extends React.Component {
     }
 
     noSleepEnable = () => {
-        console.log('No sleeping');
         this.noSleep.disable();
         this.noSleep.enable();
     }
