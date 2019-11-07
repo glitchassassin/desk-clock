eb065cbdfdc8ebb73aa6dc1678c259bf10e894d4
jwinsl01 <jonathan.winsley@fiscal.treasury.gov>
2019-11-07 17:47:43 -0500

    Adding noSleep

--- src/App.js
+++ src/App.js
@@ -6,17 +6,31 @@ import Clock from "./clock/Clock";
 import Calendar from './calendar/Calendar';
 import PomodoroTimer from "./pomodoro-timer/PomodoroTimer";
 import WorkTimer from "./work-timer/WorkTimer";
+import NoSleepJs from 'nosleep.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Backdrop/>
-      <Clock/>
-      <Calendar/>
-      <PomodoroTimer/>
-      <WorkTimer/>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.noSleep = new NoSleepJs();
+    }
+
+    noSleepEnable = () => {
+        console.log('No sleeping');
+        this.noSleep.disable();
+        this.noSleep.enable();
+    }
+
+    render() {
+        return (
+            <div className="App" onClick={this.noSleepEnable}>
+                <Backdrop/>
+                <Clock/>
+                <Calendar/>
+                <PomodoroTimer/>
+                <WorkTimer/>
+            </div>
+        );
+    }
 }
 
 export default App;
